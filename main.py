53f5d2c2e8d4260d2fe06def237f43815aaa2608
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-10 17:19:05 +0200

    fixed env

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from starlette.responses import JSONResponse
 
-from config import HOST, PORT
-from referals import get_referals_from_json
+from config import HOST, PORT, DEBUG
+from referals import get_referrals_from_json
 
 app = FastAPI()
 
@@ -15,10 +15,10 @@ async def get_root():
 
 
 @app.get('/referals')
-async def get_referals():
-    content = get_referals_from_json()
+async def get_referrals():
+    content = get_referrals_from_json()
     return JSONResponse(content=content)
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host=HOST, port=int(PORT))
+    uvicorn.run('main:app', host=HOST, port=int(PORT), reload=DEBUG)
--- referals.py
+++ referals.py
@@ -5,7 +5,7 @@ from config import PACKAGE_FOLDER
 
 
 # todo get values from table
-def get_referals_from_json() -> dict:
+def get_referrals_from_json() -> dict:
     path = Path(PACKAGE_FOLDER, 'referals.json')
     with open(path, 'rb') as file:
         texts_config = json.load(file)
