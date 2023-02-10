ad1c6facf00a070a0606de81bfbeee64848194b3
alexey_naidiuk <alexeynaidiuk@gmail.com>
2023-02-10 17:17:23 +0200

    fixed config.ini

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from configparser import ConfigParser
 import os
 import pathlib
 
@@ -5,6 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 PACKAGE_FOLDER = pathlib.Path(__file__).parent
+CONFIG = ConfigParser()
+CONFIG.read(pathlib.Path(PACKAGE_FOLDER, 'config.ini'))
+GENERAL_CONFIG = CONFIG['general']
 
-HOST = os.environ.get('HOST', '0.0.0.0')
-PORT = os.environ.get('PORT', int('8183'))
+DEBUG = os.environ.get('DEBUG', False)
+GENERAL_CONFIG['DEBUG'] = DEBUG
+DEBUG = GENERAL_CONFIG.getboolean('DEBUG')
+
+
+if not DEBUG:
+    HOST = os.environ.get('HOST', '0.0.0.0')
+    PORT = os.environ.get('PORT', 8183)
+else:
+    HOST = os.environ.get('TEST_HOST', 'localhost')
+    PORT = os.environ.get('TEST_PORT', 8283)
